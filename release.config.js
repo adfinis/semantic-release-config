a9e4e2e0ef2e870c359e52194a765dc3898ccecd
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2019-05-18 11:40:38 +0200

    feat(changelog): add changelog plugin and configure git plugin correctly

--- release.config.js
+++ release.config.js
@@ -3,8 +3,14 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/git",
     "@semantic-release/npm",
-    "@semantic-release/github"
+    "@semantic-release/github",
+    "@semantic-release/changelog",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json", "yarn.lock"]
+      }
+    ]
   ]
 };
