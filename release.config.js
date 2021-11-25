2f6636953bb453f96a633b4b034105549354716a
Jonas Metzener <jonas.metzener@adfinis.com>
2021-11-25 13:00:58 +0100

    fix(release): remove changelog generation

--- release.config.js
+++ release.config.js
@@ -5,14 +5,6 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
     "@semantic-release/github",
-    "@semantic-release/changelog",
-    [
-      "@semantic-release/git",
-      {
-        assets: ["CHANGELOG.md", "package.json"],
-        message:
-          "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}",
-      },
-    ],
+    "@semantic-release/git",
   ],
 };
