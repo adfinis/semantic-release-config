799dae46cf4547c7a2503b2dc13372b767b7a2bb
Jonas Metzener <jonas.metzener@adfinis.com>
2022-01-12 08:47:50 +0100

    feat(changelog): add changelog plugin again

--- release.config.js
+++ release.config.js
@@ -8,8 +8,9 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
     "@semantic-release/npm",
     "@semantic-release/github",
-    "@semantic-release/git",
+    ["@semantic-release/git", { assets: ["package.json", "CHANGELOG.md"] }],
   ],
 };
